6e03a69c1dd3c8c8f8c34c5e506dd3e711c91930
Faryid4780 <71910001+Faryid4780@users.noreply.github.com>
2025-03-15 18:56:51 +0800

    Update AudioAnalyticSTFTSave.py

--- AudioAnalytic/Veras/AudioAnalyticSTFTSave.py
+++ AudioAnalytic/Veras/AudioAnalyticSTFTSave.py
@@ -12,7 +12,6 @@ from os.path import exists
 
 import librosa
 import numpy as np
-import pyaudio
 
 import music_theory2 as music_theory
 
@@ -46,25 +45,13 @@ class MainProgram:
         if n_fft < n_fft_min:
             warnings.warn("每" + str(_sample_length) + "拍汉宁窗口长度" + str(n_fft) + "小于最低要求" + str(n_fft_min) + "，可能会导致不精确")
 
-        self.D = librosa.stft(y, n_fft=n_fft)  # shape[0] -> frequencies, shape[1] -> time
+        self.D = librosa.cqt(y, sr=sr, hop_length=int(n_fft/4))
         self.wave_filename = audio_file
         self.wave_file = wave.open(audio_file, 'r')
         self.samplerate = self.wave_file.getframerate()
-        freqs = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
 
-        # 寻找距离tet最近的freqs项，输出到nearest_indices
-        # 初始化一个空列表来存储最近邻索引
-        nearest_indices = []
 
-        # 对于 tet 中的每个频率值
-        for freq in tet:
-            # 计算 freqs 中距离 freq 最近的索引
-            nearest_index = np.argmin(np.abs(freqs - freq))
-
-            # 将最近的索引添加到列表中
-            nearest_indices.append(nearest_index)
-
-        self.piano_D = dict(zip(all_phonic_num, np.abs(self.D[nearest_indices])))
+        self.piano_D = dict(zip(all_phonic_num, np.abs(self.D)))
         self.path = "stft_data\\" + audio_file.split("\\")[-1] + ".dat"
         wf = self.wave_file
         self.datasize = wf.getsampwidth() * wf.getnchannels()
@@ -77,16 +64,15 @@ class MainProgram:
                                   output=True)
 
     def save_stft_data(self):
-        print("Saving stft file...")
+        print("Saving cqt file...")
 
         # 时间点对应的stft数据
         time_to_stft = {}
 
-        chord_list = []
-
         a = np.linspace(0, self.sum_tl, self.D.shape[1])
         for x in range(self.D.shape[1]):
             stft_data = np.array([a[x] for a in self.piano_D.values()])  # stft输入
+            print(stft_data.shape)
             # piano_D
             # {'A4':[time1:..., time2:..., time3:..., ...]
             # 'Bb4':[time1:..., time2:..., time3:..., ...]}
@@ -100,7 +86,7 @@ class MainProgram:
         pickle.dump(time_to_stft, save_music_stft)
         save_music_stft.close()
         self.wave_file.close()
-        print("STFT data has saved to", self.path)
+        print("CQT data has saved to", self.path)
         return self.path
 
     def __play_sound(self, st, et):
@@ -118,16 +104,18 @@ class NewMainProgram:
         self.se_l = start_end_list
 
         self.wave_file = wave.open(open(audio_file, 'rb'))
+        self.filename = audio_file
 
         self.framerate = self.wave_file.getframerate()
         self.sampwidth = self.wave_file.getsampwidth()
+        self.nframes = self.wave_file.getnframes()
         self.datasize = self.sampwidth * self.wave_file.getnchannels()
 
     def read_specific_frames(self, start_frame: int, end_frame: int):
         start_frame -= start_frame % self.datasize
         end_frame -= end_frame % self.datasize
-        start_frame = int(start_frame)
-        end_frame = int(end_frame)
+        start_frame = max(int(start_frame), 0)
+        end_frame = min(int(end_frame), self.nframes)
         data = None
         try:
             self.wave_file.setpos(start_frame)
@@ -137,43 +125,52 @@ class NewMainProgram:
 
         return data
 
-    def record(self) -> list:
+    def record(self, timesteps=6, neighbors=2) -> list:
+        print("Recording...", self.filename)
         recordings = []
+        loc = 0
+        length = len(self.se_l)
+        now = 0
+
+        mul = 50
+        print('[' + '=' * mul + ']')
+        print('-', end='')
+
         for tupl in self.se_l:
             assert isinstance(tupl, tuple)
 
-            wave_data = self.read_specific_frames(tupl[0] * self.framerate,
-                                                  tupl[1] * self.framerate)
+            # 获取音频数据
+            wave_data_length = int((tupl[1] - tupl[0]) * self.framerate)
+            neighbor_length = wave_data_length // timesteps * neighbors
+            wave_data = self.read_specific_frames(tupl[0] * self.framerate - neighbor_length,
+                                                  tupl[1] * self.framerate + neighbor_length)
 
             if wave_data is None:
                 break
 
             fmt = f'<{len(wave_data) // self.sampwidth}h'  # '<h' means little-endian 16-bit signed integer
             amplitudes = struct.unpack(fmt, wave_data)
-            fft_data = np.abs(np.fft.fft(amplitudes))
-            fft_freq = np.fft.fftfreq(len(amplitudes))
 
-            positive_freq_indices = np.where(fft_freq >= 0)
-            fft_data_positive = fft_data[positive_freq_indices]
-            fft_freq_positive = fft_freq[positive_freq_indices] * self.framerate
+            # 将获取的幅度数据用于 CQT
+            y = np.array(amplitudes, dtype=float)  # amplitudes 是从原始音频数据中解包出来的幅度数据
+            # 使用 librosa.cqt 进行常数Q变换（CQT）
+            cqt_result = librosa.cqt(y, sr=self.framerate, n_bins=88, bins_per_octave=12,
+                                     hop_length=len(amplitudes) // (timesteps - 1)).T
+            # 记录下来
+            recordings.append(np.abs(cqt_result))
 
-            # plt.figure(figsize=(10, 6))
-            # plt.plot(fft_freq_positive, fft_data_positive)
-            # plt.title('Frequency Spectrum')
-            # plt.xlabel('Frequency (Hz)')
-            # plt.ylabel('Amplitude')
-            # plt.grid()
-            # plt.show()
-            piano_fft = fft_data_positive[[find_nearest(freq, fft_freq_positive) for freq in tet]]
-            recordings.append(piano_fft)
-        recordings = np.array(recordings)
-        return recordings/np.mean(recordings)
+            if loc / length * 50 > now:
+                print('=', end='')
+                now += 1
+            loc += 1
+        print('>Done')
+        return np.array(recordings) / np.mean(recordings)
 
     def close(self):
         self.wave_file.close()
 
 
 if __name__ == '__main__':
-    aa = NewMainProgram("youtube_downloaded\\H87GqJujcOk.wav", ((23, 24),))
-    aa.record()
+    aa = NewMainProgram("youtube_downloaded\\H87GqJujcOk.wav", ((23, 24),(24, 26),(25.7, 27.5), (33.4, 38.7)))
     aa.close()
+    
